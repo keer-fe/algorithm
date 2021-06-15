69f690bf286b99766bb8be95be026bb14e43995c
sunshumin <ssmin3@163.com>
2021-06-15 21:01:53 +0800

    2.两数相加.js

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,27 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
+const addTwoNumbers = (l1, l2) => {
+  let carry = 0
+  let result = new ListNode(-1)
+  let current = result
 
-};
+  while(l1 || l2) {
+    const val1 = l1 ? l1.val : 0
+    const val2 = l2 ? l2.val : 0
+
+    current.next = new ListNode((val1 + val2 + carry) % 10)
+    carry = Math.floor((val1 + val2 + carry) / 10)
+
+    l1 = l1 ? l1.next : l1
+    l2 = l2 ? l2.next : l2
+
+    current = current.next
+  }
+
+  if (carry) current.next = new ListNode(carry)
+
+  return result.next
+}
 // @lc code=end
 
